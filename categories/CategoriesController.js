29ef8e7b4c96531df3e81a776bbf29c8a7dfa6a9
Patrick <patriick.dev@hotmail.com>
2025-06-27 11:23:19 -0300

    feat: cadastro de categorias no db e slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -2,11 +2,30 @@
 const express = require("express")
 //objeto que será utilizado para criar as rotas
 const router = express.Router()
+//carregando model
+const Category = require("./Category")
+const slugify = require("slugify")
 
 //rota para criar nova categoria
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new")
 })
 
+//rota para salvar as categorias no banco de dados
+router.post("/categories/save", (req, res) => {
+    let title = req.body.title
+    if (title != undefined) {
+
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect("/")
+        })
+
+    } else {                                    //caso o usuario digite um valor nullo
+        res.redirect("/admin/categories/new")
+    }
+})
 
 module.exports = router
