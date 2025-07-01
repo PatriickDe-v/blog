0364dd8ec70732eb7a8bf4d2c657b5be28bed8a7
Patrick <patriick.dev@hotmail.com>
2025-07-01 11:47:27 -0300

    feat: formulario de edição + links de navegação

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -20,7 +20,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title)
         }).then(() => {
-            res.redirect("/")
+            res.redirect("/admin/categories")
         })
 
     } else {                                    //caso o usuario digite um valor nullo
@@ -56,5 +56,26 @@ router.post("/categories/delete", (req, res) => {
     } else { //se for null
         res.redirect("/admin/categories")
     }
+})
+
+//rota para edição de categoria
+router.get("/admin/categories/edit/:id", (req, res) => {
+    let id = req.params.id
+    
+    if(isNaN(id)) {
+        res.redirect("admin/categories")
+    }
+
+    Category.findByPk(id).then(category => {
+        if(category != undefined) {
+
+        res.render("admin/categories/edit", {category: category})
+
+        }else {
+            res.redirect("/admin/categories")
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories")
+    })
 })
 module.exports = router
