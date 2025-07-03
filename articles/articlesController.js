90f6b91f876c5e09792fa25b263e1e8721485217
Patrick <patriick.dev@hotmail.com>
2025-07-03 15:32:26 -0300

    feat: autorização com middleware

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -9,12 +9,12 @@ const Article = require("./Article")
 //carregando slug
 const slugify = require("slugify")
 
-
+const adminAuth = require("../middlewares/adminAuth")
 
 
 
 //rota de categorias
-router.get("/admin/articles", (req, res) => {
+router.get("/admin/articles", adminAuth, (req, res) => {
     Article.findAll({
         include: [{ model: Category }]
     }).then(articles => {
@@ -22,7 +22,7 @@ router.get("/admin/articles", (req, res) => {
     })
 })
 
-router.get("/admin/articles/new", (req, res) => {
+router.get("/admin/articles/new", adminAuth, (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", { categories: categories })
     })
@@ -67,7 +67,7 @@ router.post("/articles/delete", (req, res) => {
 })
 
 //rota para editar artigos
-router.get("/admin/articles/edit/:id", (req, res) => {
+router.get("/admin/articles/edit/:id", adminAuth, (req, res) => {
     let id = req.params.id
     Article.findByPk(id).then(article => {
         if (article != undefined) {
