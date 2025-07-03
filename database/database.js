95e118483c55a1e358854f950e54ebbeb421aa76
Patrick <patriick.dev@hotmail.com>
2025-07-03 09:00:22 -0300

    refactor: atualização do código em versão mais nova

--- database/database.js
+++ database/database.js
@@ -5,7 +5,8 @@ const Sequelize = require("sequelize")
 const connection = new Sequelize('blog', 'root', '123123', {
     host: 'localhost',
     dialect: 'mysql',
-    timezone: "-03:00"
+    timezone: "-03:00",
+    logging: false
 })
 
 module.exports = connection
