bbdfa9c96fa586e20d048f91b47064635618ef8c
Patrick <patriick.dev@hotmail.com>
2025-07-03 14:32:46 -0300

    feat: verificando emails duplicados

--- user/usersController.js
+++ user/usersController.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcryptjs")
 
 router.get("/admin/users", (req, res) => {
     User.findAll().then(users => {
-        res.render("admin/users/index", {users: users})
+        res.render("admin/users/index", { users: users })
     })
 })
 
@@ -16,6 +16,29 @@ router.get("/admin/users/create", (req, res) => {
 router.post("/users/create", (req, res) => {
     let email = req.body.email
     let password = req.body.password
+
+    //logica para emails duplicados
+    User.findOne({ where: { email: email } }).then(user => {
+        if (user == undefined) {
+            //hash de senha
+            let salt = bcrypt.genSaltSync(10)
+            let hash = bcrypt.hashSync(password, salt)
+
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect("/")
+            }).catch(err => {
+                res.redirect("/")
+            })
+        } else {
+            res.redirect("/admin/user/create")
+        }
+    })
+
+
+
 
     //hash de senha
     let salt = bcrypt.genSaltSync(10)
